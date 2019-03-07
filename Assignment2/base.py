Fixed Unstable Softmax

Assignment2/base.py
@@ -69,9 +69,14 @@ class BaseFramework:
         self.init_action_value_f()
         self.hist.reset()
     
-    def _get_softmax_probas(self,obs):
-        z = np.exp(self.get_action_state_value(obs)/self.temperature)
-        return z / np.sum(z)
+    def _get_softmax_probas(self,obs,use_temp=True):
+        # we substracted the max to stabilize the softmax for small values of tempertures and positive large values of state-values
+        mx = max(self.get_action_state_value(obs))
+        if use_temp:
+            z = (self.get_action_state_value(obs)- mx)/self.temperature
+        else:
+            z = self.get_action_state_value(obs) - mx
+        return np.exp(z) / np.sum(np.exp(z))
     
     def get_action(self, obs, greedy=False):
         if not greedy and self.mode == "train":

Assignment2/models.py
@@ -66,5 +66,5 @@ class QLearning(BaseDiscrete):
 
 class ExpectedSARSA(QLearning):
     def _get_next_q(self,obs):
-        return np.sum(self._get_softmax_probas(obs)*self.get_action_state_value(obs))
+        return np.sum(self._get_softmax_probas(obs,use_temp=False)*self.get_action_state_value(obs))
 